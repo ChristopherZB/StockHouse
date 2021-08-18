e92d3e34c9e42b2189c6b6baf81ca823b836e698
BennettCZ <bennetcz5682@uwec.edu>
2021-08-18 18:52:32 -0500

    Inital commit

---
+++ Templates/FormChildren/FieldClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageTemplates.Templates.FormChildren
+{
+    public class FieldClass<TValue> : ComponentBase
+    {
+
+        [Parameter]
+        public TValue? Value { 
+            get; 
+            set; }
+        [Parameter]
+        public string Label { get; set; }
+        [Parameter]
+        public EventCallback<TValue> ValueChanged { get; set; }
+
+        public virtual async Task SetValue(ChangeEventArgs args)
+        {
+            await SetValue((TValue)args.Value);
+        }
+
+        public virtual async Task SetValue(TValue val)
+        {
+            Value = val;
+            await ValueChanged.InvokeAsync(Value);
+            StateHasChanged();
+        }
+    }
+}
---
+++ Templates/FormChildren/IntField.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageTemplates.Templates.FormChildren.Partials
+{
+    public partial class IntField : FieldClass<int>
+    {
+        public override async Task SetValue(ChangeEventArgs args)
+        {
+            int val;
+            if (!int.TryParse((string)args.Value, out val))
+                val = -1;
+
+            await SetValue(val);
+        }
+
+    }
+}
