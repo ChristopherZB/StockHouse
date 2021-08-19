d29b85d9921621124e11cecd70d0d6dccafce8a5
BennettCZ <bennetcz5682@uwec.edu>
2021-08-19 15:32:45 -0500

    Adding in table component

---
+++ Data/TestObject.cs
@@ -0,0 +1,17 @@
+﻿using StockHouse.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockHouse.Data
+{
+    public class TestObject
+    {
+        [DisplayOnTable]
+        public string TestName { get; set; }
+        [DisplayOnTable("Yeehaw")]
+        public int TestInt { get; set; }
+        public string AnotherTest { get; set; }
+    }
+}
---
+++ Helpers/DisplayOnTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockHouse.Helpers
+{
+    public class DisplayOnTable : Attribute
+    {
+        public bool UseFieldName;
+        public string DisplayName;
+
+        public DisplayOnTable()
+        {
+            UseFieldName = true;
+        }
+        public DisplayOnTable(string displayName)
+        {
+            UseFieldName = false;
+            DisplayName = displayName;
+        }
+    }
+}
