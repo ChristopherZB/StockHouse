e8d3a43faec17a9eb68e0e5fc527ca995a1d99ca
Kyle Sargeant <Sargeakb9348@uwec.edu>
2021-08-20 17:52:56 -0500

    Added attributes to StockObject to display data fields on table.

--- Data/StockObject.cs
+++ Data/StockObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockHouse.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,16 +11,27 @@ namespace StockHouse.Data
         //stock object fields 
         public int StockID { get; set; }
 
+        [DisplayOnTable("Name")]
         public string StockName { get; set; }//retrival from get method in NameObject
 
-        public string StockItemNum { get; set; }//retrival from itemNum textfield
+
         
+        public string StockItemNum { get; set; }//retrival from itemNum textfield
+
+
+        [DisplayOnTable("Item Name")]
         public string StockItemName { get; set; }//retrival from itemName textfield
+
+        [DisplayOnTable("Location")]
 
         public string StockLocation { get; set; }//retrival from get method in LocationObject
 
+
+        [DisplayOnTable("Quantity")]
         public int StockQuantity { get; set; }// retrival from quantity counter thing???
 
+
+        [DisplayOnTable("Time Added")]
         public DateTime StockDateTime { get; set; }
     }
 }
