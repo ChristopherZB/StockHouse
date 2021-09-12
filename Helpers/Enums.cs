4bd08fe7e45bf824482005ec91e09af6c33c14ca
BennettCZ <bennetcz5682@uwec.edu>
2021-09-12 15:05:36 -0500

    Added stockfields enum

---
+++ Helpers/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockHouse.Helpers
+{
+    public enum StockFields
+    {
+        Name,
+        Location,
+        Item,
+        ItemNumber
+    }
+}
